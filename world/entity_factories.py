81ddac08720edfe2b1dd51eb920781dda2d0703f
Mag Rat <magrat@pop-os.localdomain>
2020-03-03 15:41:05 +1300

    Blarga

--- world/entity_factories.py
+++ world/entity_factories.py
@@ -1,6 +1,29 @@
 import libtcodpy as libtcod
 import random
 
+class NPCFactory:
+    def __init__(self, type, level):
+        self.type = type
+        self.level = level
+
+    def get_monster(x, y):
+        tmp_entity = {
+                    "X"             :       x,
+                    "Y"             :       y,
+                    "CHAR"          :       "G",
+                    "COLOR"         :       libtcod.green,
+                    "NAME"          :       "Goblin",
+                    "BLOCKS"        :       True,
+                    "FIGHTER"       :       True,
+                    "FIGHTER_HP"    :       2,
+                    "FIGHTER_DEF"   :       0,
+                    "FIGHTER_ATK" :       1,
+                    "AI"            :       True,
+                    "AI_PACKAGE"    :       "Chase"
+                    }
+        return tmp_entity
+
+
 class MonsterFactory:
     def __init__(self, type, level):
         self.type = type
