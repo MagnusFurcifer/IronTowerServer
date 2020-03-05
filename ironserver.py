2f48ed87e4a1a0d3d5a92396e4eb6d8dc55057a5
Mag Rat <magrat@pop-os.localdomain>
2020-03-05 21:38:58 +1300

    Blarga

--- ironserver.py
+++ ironserver.py
@@ -53,7 +53,7 @@ def get_events(conn):
 #Web server
 def start_web_server():
     os.chdir(it_config.ironweb_path)
-    with socketserver.TCPServer(('', it_config.ironweb_port), http.server.SimpleHttpRequestHandler) as httpd:
+    with socketserver.TCPServer(('', it_config.ironweb_port), http.server.SimpleHTTPRequestHandler) as httpd:
         httpd.serve_forever()
 
 def worker_proc():
