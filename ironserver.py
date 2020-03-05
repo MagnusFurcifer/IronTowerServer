4b9f0df9c7e89f20aa778f76b3991c3e7374b01a
Mag Rat <magrat@pop-os.localdomain>
2020-03-05 22:23:42 +1300

    Blarga

--- ironserver.py
+++ ironserver.py
@@ -23,6 +23,7 @@ def create_con():
     return conn
 
 def insert_event(conn, event_text, event_date):
+    print("We doin a cheeky insert: " + event_text + event_date)
     c = conn.cursor()
     c.execute("INSERT INTO events (event_text, event_date) VALUES (?, ?);", (event_text, event_date))
 
