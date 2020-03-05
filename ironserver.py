c5e810cd6c4dc6dddd213534f2d841aa9f4cb066
Mag Rat <magrat@pop-os.localdomain>
2020-03-05 23:04:57 +1300

    Blarga

--- ironserver.py
+++ ironserver.py
@@ -84,8 +84,9 @@ async def echo_server(reader, writer):
         event = "Map generated at: " + str(command.get("TYPE")) + " on level: " + str(command.get("LEVEL"))
         writer.write(json.dumps(gendmap).encode())
         await writer.drain()  # Flow control, see later
-    elif command.get("COMMAND") == "ASCEND":
-        event = "Player has ascneded to level " + str(command.get("ASC_LEVEL")) + " of the " + str(command.get("ASC_TYPE"))
+    elif command.get("COMMAND") == "EVENT":
+        if command.get("TYPE") == "ASCEND":
+            event = "Player has ascneded to level " + str(command.get("ASC_LEVEL")) + " of the " + str(command.get("ASC_TYPE"))
     writer.close()
     if event is not None:
         print("Inserting event: " + event)
