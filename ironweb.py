347c569f31137dd951be438a1acebce8a632d12d
Mag Rat <magrat@pop-os.localdomain>
2020-03-05 22:13:50 +1300

    Blarga

--- ironweb.py
+++ ironweb.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 import it_config
+import os
 
 os.chdir(it_config.ironweb_path)
 with socketserver.TCPServer(('', it_config.ironweb_port), http.server.SimpleHTTPRequestHandler) as httpd:
