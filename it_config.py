09b9be757bd1384fa98dccb4144b4d9f1ee7131f
Mag Rat <magrat@pop-os.localdomain>
2020-03-05 22:04:55 +1300

    Blarga

--- it_config.py
+++ it_config.py
@@ -1,4 +1,5 @@
 import libtcodpy as libtcod
+import os
 
 random_seed = "IRONTOWER"
 
@@ -24,5 +25,6 @@ colors = {
 ironserver_host = '0.0.0.0'  # The server's hostname or IP address
 ironserver_port = 8080        # The port used by the server
 ironweb_port = 8000 #Web server to display logs
+iron_root = os.getcwd()
 ironweb_path = "./httpd/"
 db_path = "ironserver.db"
